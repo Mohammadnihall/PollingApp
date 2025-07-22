c52abf600d193c80925131860bb19b1bd358b8c0
Mohammadnihall <Mohammadnihalteesmsc@gmail.com>
2025-07-22 23:43:46 +0530

    Implemented screens to view poll details and submit the vote for the polls and see the polls with the submitted options

--- app/src/main/java/project/app/pollingapp/HomeScreen.kt
+++ app/src/main/java/project/app/pollingapp/HomeScreen.kt
@@ -26,4 +26,22 @@ class HomeScreen {
             modifier = Modifier.padding(bottom = 24.dp)
         )
     }
+
+    Column(
+    modifier = Modifier
+    .fillMaxWidth(0.9f)
+    .clip(RoundedCornerShape(20.dp))
+    .background(MaterialTheme.colorScheme.surface)
+    .padding(24.dp), // Add elevation for a card-like effect
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+    )
+    {
+        Text(
+            text = "Create Account",
+            style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+    }
 }
